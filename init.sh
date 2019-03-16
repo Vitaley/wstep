56c3ced057d70765728736f940a977ea7a1549b8
Vitaley <48580555+Vitaley@users.noreply.github.com>
2019-03-17 00:48:01 +0500

    unicorn

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:app_me
+gunicorn -b 0.0.0.0:8080 hello:app_me&
 #sudo /etc/init.d/mysql start
